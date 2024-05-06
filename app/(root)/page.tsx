41c74bd78014d50798fb4f88dd6cdddaba03c6e5
Alwina Harutyunyan <alvina.harutyunyan@qiibee.com>
2024-05-06 16:27:03 +0400

    add chartjs first chart

---
+++ app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import HeaderBox from '../../components/HeaderBox'
+import TotalBalanceBox from "../../components/TotalBalanceBox";
+
+const Home = () => {
+    const loggedIn = { firstName: 'Alwi' }
+    return (
+        <section className='home'>
+            <div className='home-content'>
+                <header className="home-header">
+                    <HeaderBox
+                        type='greeting'
+                        title='Welcome'
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext='Access and manage your account and transactions efficiently.'
+                    />
+
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={9999.99}
+                    />
+                </header>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
